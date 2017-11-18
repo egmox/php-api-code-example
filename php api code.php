62ea8571fe784a2b350fe31df3fe1bb0c2079e33
Gulshan <egmox2@gmail.com>
2017-11-19 02:17:18 +0530

    PHP-API code sample
    Those who were having problem with developing PHP-API for their Android/Java apps, now you can do it easily.
    
    Just tweak the code according to your requirement.

---
+++ php api code.php
@@ -0,0 +1,9 @@
+<?php
+include('db.php');
+$var = $_POST['username']);
+
+echo $var;//here is the response that we can get in java/android
+//to send json from php to java/json, the code is something like
+//json_encode($var("key","value"));
+
+?>
